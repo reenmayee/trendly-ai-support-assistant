b925859a2751a7b32241f89a08f9f899e57f59d9
reenmayee <158175578+reenmayee@users.noreply.github.com>
2026-08-25 02:15:04 +0530

    Fix import statement for text splitter module

--- rag.py
+++ rag.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 
